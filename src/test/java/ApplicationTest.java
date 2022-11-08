5f3eaed7991419fc176253b92554ce958fd94c9e
TaeMin <xoals3094@naver.com>
2022-11-08 20:46:17 +0900

    test: getRestStringTest 오타 수정

--- src/test/java/ApplicationTest.java
+++ src/test/java/ApplicationTest.java
@@ -33,16 +33,15 @@ public class ApplicationTest {
 
     @DisplayName("공통 변수형 부분 제외, 나머지 문자열 반환 테스트")
     @Nested
-    class getRestStringTest{
+    class getRestString {
         @Test
-        void case1(){
+        void case1() {
             int commonTypeDivisionIndex = 4;
             String input = "int& a*[]&, b, c*;";
             String result = " a*[]&, b, c*;";
 
             assertThat(Application.getRestString(input, commonTypeDivisionIndex))
-                    .isEqaulTo(result);
+                    .isEqualTo(result);
         }
     }
-
 }
