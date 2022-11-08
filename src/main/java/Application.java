6ffe938030437295a4cd2f04fb97ab775858398a
TaeMin <xoals3094@naver.com>
2022-11-09 01:01:19 +0900

    완성

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -3,9 +3,9 @@ import java.util.*;
 public class Application {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        String input = scanner.next();
+        String input = scanner.nextLine();
 
-        List<String> oneLineVariable = getOneLineVariable("int& a*[]&, b, c*;");
+        List<String> oneLineVariable = getOneLineVariable(input);
         for (String variable : oneLineVariable) {
             System.out.println(variable);
         }
