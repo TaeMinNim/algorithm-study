625de5d41fa3921e6406abbef6405fc686e473df
TaeMin <xoals3094@naver.com>
2022-11-08 18:35:47 +0900

    test: getCommonTypeTest 테스트 추가

--- src/test/java/ApplicationTest.java
+++ src/test/java/ApplicationTest.java
@@ -1,16 +1,33 @@
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
+
 import static org.assertj.core.api.Assertions.*;
 
 public class ApplicationTest {
     @DisplayName("공통타입 구분자 인덱스 위치 반환 테스트")
     @Nested
-    class findCommonTypeDivisionIndexTest{
+    class findCommonTypeDivisionIndexTest {
         @Test
-        void case1(){
+        void case1() {
             String input = "int& a*[]&, b, c*;";
-            assertThat(Application.findCommonTypeDivisionIndex(input)).isEqualTo(4);
+            int result = 4;
+
+            assertThat(Application.findCommonTypeDivisionIndex(input))
+                    .isEqualTo(result);
+        }
+    }
+
+    @DisplayName("공통 변수형 잘라내기 테스트")
+    @Nested
+    class getCommonTypeTest {
+        @Test
+        void case1() {
+            int commonTypeDivisionIndex = 4;
+            String result = "int&";
+
+            assertThat(Application.getCommonType(commonTypeDivisionIndex)
+                    .isEqualTo(result);
         }
     }
 }
